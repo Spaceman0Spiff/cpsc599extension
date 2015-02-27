51d342c218bb37ef2d9796bff7bf52d838c4e443
Spaceman0Spiff <SpacemanSpiff@Spaceman-Spiff.local>
2015-02-27 15:53:45 -0700

    my new change
    jameson

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 $("div#watch7-sidebar").prepend('<div class="watch-sidebar-gutter   yt-card yt-card-has-padding    yt-uix-expander yt-uix-expander-collapsed">Test</div>');
 
-//testing
+//my new test
