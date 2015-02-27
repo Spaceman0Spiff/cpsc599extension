b9a679bdbebe957d7fb1019c08585d7813f816b3
MarkPolivchuk <mark.polivchuk@gmail.com>
2015-02-27 16:09:25 -0700

    3 divs
    dfg;uhsadoifs

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
-$("div#watch7-sidebar").prepend('<div class="watch-sidebar-gutter   yt-card yt-card-has-padding    yt-uix-expander yt-uix-expander-collapsed">Test</div>');
+$("div#watch7-sidebar").prepend('<div class="watch-sidebar-gutter   yt-card yt-card-has-padding    yt-uix-expander yt-uix-expander-collapsed">Test1</div>');
+$("div#watch7-sidebar").prepend('<div class="watch-sidebar-gutter   yt-card yt-card-has-padding    yt-uix-expander yt-uix-expander-collapsed">Test2</div>');
+$("div#watch7-sidebar").prepend('<div class="watch-sidebar-gutter   yt-card yt-card-has-padding    yt-uix-expander yt-uix-expander-collapsed">Test3</div>');
 
 //my new test
