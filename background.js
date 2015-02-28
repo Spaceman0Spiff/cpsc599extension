8183c0e1904f81642fe2c8c49586c6affae9c3fd
MarkPolivchuk <mark.polivchuk@gmail.com>
2015-02-28 14:39:17 -0700

    Added container and header (plus css file)
    on my computer pushing the related videos 494 pixels lines it up
    perfectly. this can probably be changed in code for different sized
    screens

--- background.js
+++ background.js
@@ -3,12 +3,16 @@
 var i = 1;
 var j = 1;
 
+// adds container and header
+$('#watch7-sidebar').prepend('<div id="comment-container" class="watch-sidebar comment-container"></div>');
+$('#comment-container').before('<div id="comment-header"><p class="comment-header"><strong>Comments</strong></p></div>');
+
 $(document).ready(function() {
 			auto_refresh();
 		});
 		function auto_refresh(){
 			var randomnumber = Math.floor(Math.random() * 100);
-			var comment = $("div#watch7-sidebar").prepend('<div id="'+i+'" class="watch-sidebar-gutter   yt-card yt-card-has-padding    yt-uix-expander yt-uix-expander-collapsed">Test ' + randomnumber + '</div>'); //inserts a comment every second
+			var comment = $('#comment-container').prepend('<div id="'+i+'" class="watch-sidebar-gutter   yt-card yt-card-has-padding    yt-uix-expander yt-uix-expander-collapsed">Test ' + randomnumber + '</div>'); //inserts a comment every second
             i++;
             setTimeout(function (){
                 $( 'div#' + j ).remove(); //deletes that comment 7 seconds later
