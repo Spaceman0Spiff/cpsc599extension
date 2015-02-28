2d52eebb41c8f91686c0cafd9bcd57cc68d8333b
Spaceman0Spiff <SpacemanSpiff@Spaceman-Spiff.local>
2015-02-28 11:01:05 -0700

    dynamic comments added every second
    a comment is added, scrolls down \ until it is deleted 9 secs later

--- background.js
+++ background.js
@@ -1,5 +1,22 @@
-$("div#watch7-sidebar").prepend('<div class="watch-sidebar-gutter   yt-card yt-card-has-padding    yt-uix-expander yt-uix-expander-collapsed">Test1</div>');
-$("div#watch7-sidebar").prepend('<div class="watch-sidebar-gutter   yt-card yt-card-has-padding    yt-uix-expander yt-uix-expander-collapsed">Test2</div>');
-$("div#watch7-sidebar").prepend('<div class="watch-sidebar-gutter   yt-card yt-card-has-padding    yt-uix-expander yt-uix-expander-collapsed">Test3</div>');
+//http://crunchify.com/how-to-refresh-div-content-without-reloading-page-using-jquery-and-ajax/
 
-//my new test
+var i = 1;
+var j = 1;
+
+$(document).ready(function() {
+			auto_refresh();
+		});
+		function auto_refresh(){
+			var randomnumber = Math.floor(Math.random() * 100);
+			var comment = $("div#watch7-sidebar").prepend('<div id="'+i+'" class="watch-sidebar-gutter   yt-card yt-card-has-padding    yt-uix-expander yt-uix-expander-collapsed">Test ' + randomnumber + '</div>'); //inserts a comment every second
+            i++;
+            setTimeout(function (){
+                $( 'div#' + j ).remove(); //deletes that comment 7 seconds later
+                j++;
+            }, 9000);
+		}
+		var refreshId = setInterval(auto_refresh, 1000);
+
+
+
+
